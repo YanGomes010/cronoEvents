a5e6a121a9ee4d1658045a97d3813f34f20268e0
Yan Gomes <yangomesmd@gmail.com>
2024-12-07 19:18:35 -0400

    Adicionando menu de dashboard

--- src/Components/ListItem/index.js
+++ src/Components/ListItem/index.js
@@ -1,28 +1,29 @@
 import styled from "styled-components"
 
-export default function ListItem({imagem}){
-    return(
+export default function ListItem({ imagem, title }) {
+    return (
         <Li>
-            <img src={imagem} alt="icone"/>
-            <p>Inicio</p>
+            <img src={imagem} alt="icone" />
+            <p>{title}</p>
         </Li>
     )
 }
 
 const Li = styled.li`
     display: flex;
-    width:109px;
-    max-width:130px;
-padding: 2px 8px;
-justify-content: space-around;
-align-items: center;
-gap: 7px;
-border-radius: 6px;
-background: #5C4033;
-color: #FFF8E7;
-font-family: "Roboto Condensed";
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
+    width:100%;
+    max-width:120px;
+    padding: 4px 10px;
+    justify-content: space-around;
+    align-items: center;
+    gap: 7px;
+    border-radius: 6px;
+    background: #5C4033;
+    color: #FFF8E7;
+    font-family: "Roboto Condensed";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    cursor:pointer;
 `
